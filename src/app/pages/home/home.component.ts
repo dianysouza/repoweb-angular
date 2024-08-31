1557d61d019f6a890bf22f4732c8cebd2c352608
dianysouza <diany.06@gmail.com>
2024-08-31 18:14:51 -0300

    alteracoes na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Projects } from '../../interfaces/projects';
+import { ProjectsService } from '../../services/projects.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,55 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  listProjectsByID: Projects[] = [];
+  listProjectsByAccess: Projects[] = [];
+
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit()
+  {
+    this.listAllProjectsByAccess();
+    this.listAllProjectsById();
+    //this.projeto();
+  }
+
+  listAllProjectsById()
+  {
+    this.projectsService.listProjects().subscribe({
+      next: (response: any) => {
+        this.listProjectsByID = response;
+        this.listProjectsByID = this.listProjectsByID.sort((a,b) => b.cd_projeto - a.cd_projeto);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  listAllProjectsByAccess()
+  {
+    this.projectsService.listProjects().subscribe({
+      next: (response: any) => {
+        this.listProjectsByAccess = response;
+        this.listProjectsByAccess = this.listProjectsByAccess.sort((a,b) => b.qt_acesso - a.qt_acesso);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  /*projeto()
+  {
+    this.projectsService.getProject(2).subscribe({
+      next: (response: any) => {
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }*/
 
 }
